7b71f85ea73a4c319c1110a3aa3b1e33e214c0f3
TayfurSafakGencay <safak.gencay@gmail.com>
2024-07-11 11:35:36 +0300

    New: Click & Game Manager

---
+++ Assets/Scripts/Managers/ClickManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using View;
+
+namespace Managers
+{
+  public class ClickManager : MonoBehaviour
+  {
+    [SerializeField]
+    private Camera _camera;
+    
+    private const string _car = "Car";
+    private void Update()
+    {
+      if (!Input.GetMouseButtonDown(0)) return;
+      Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+      if (!Physics.Raycast(ray, out RaycastHit hit)) return;
+      if (!hit.collider.CompareTag(_car)) return;
+
+      bool hasItCarComponent = hit.collider.TryGetComponent(out Car car);
+
+      if (!hasItCarComponent)
+      {
+        Debug.LogError("Object has not Car component!");        
+        return;
+      }
+      
+      car.Move();
+    }
+  }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Managers
+{
+  public class GameManager : MonoBehaviour
+  {
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+      if (Instance == null) Instance = this;
+    }
+
+    private void Start()
+    {
+      GameStarted();
+    }
+
+    public static Action OnGameStarted;
+    
+    public void GameStarted()
+    {
+      OnGameStarted?.Invoke();  
+    }
+
+    public static Action<bool> OnGameFinished;
+
+    public void GameFinished(bool success)
+    {
+      OnGameFinished?.Invoke(success);
+    }
+  }
+}
