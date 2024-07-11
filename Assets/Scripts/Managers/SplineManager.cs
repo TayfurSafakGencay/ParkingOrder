7026171222e9c9829d10270b565ecb30587dc272
TayfurSafakGencay <safak.gencay@gmail.com>
2024-07-11 11:34:49 +0300

    New: Spline & Spline Manager

---
+++ Assets/Scripts/Managers/SplineManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Dreamteck.Splines;
+using UnityEngine;
+using Spline = View.Spline;
+
+namespace Managers
+{
+  public class SplineManager : MonoBehaviour
+  {
+    public static SplineManager Instance;
+
+    [SerializeField]
+    private Material _material;
+
+    [SerializeField]
+    private List<Color> _colors;
+
+    private void Awake()
+    {
+      if (Instance == null) Instance = this;
+    }
+
+    public void AddSpline(SplineComputer splineComputer, Spline spline)
+    {
+      Material material = new(_material);
+      Color color = GetRandomColor();
+      material.color = color;
+
+      spline.SetColors(color);
+      
+      CarManager.Instance.CreateCar(splineComputer, material);
+    }
+
+    private Color GetRandomColor()
+    {
+      System.Random random = new();
+      int index = random.Next(_colors.Count);
+      return _colors[index];
+    }
+
+  }
+}
---
+++ Assets/Scripts/View/Spline.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Dreamteck.Splines;
+using Managers;
+using UnityEngine;
+
+namespace View
+{
+  public class Spline : MonoBehaviour
+  {
+    private SplineComputer _splineComputer;
+
+    [SerializeField]
+    private List<MeshRenderer> _parkAreaLines;
+
+    private void Awake()
+    {
+      _splineComputer = gameObject.GetComponent<SplineComputer>();
+      
+      SubscribeActions();
+    }
+
+    private void SubscribeActions()
+    {
+      GameManager.OnGameStarted += OnGameStarted;
+    }
+
+    private void OnGameStarted()
+    {
+      SplineManager.Instance.AddSpline(_splineComputer, this);
+
+    }
+
+    public void SetColors(Color color)
+    {
+      // ColorAllTheDots(color);
+      ColorParkAreaLines(color);
+    }
+    
+    private void ColorAllTheDots(Color color)
+    {
+      SplinePoint[] splinePoints = _splineComputer.GetPoints();
+
+      for (int i = 0; i < splinePoints.Length; i++)
+      {
+        _splineComputer.SetPointColor(i, color);
+      }
+    }
+
+    private void ColorParkAreaLines(Color color)
+    {
+      for (int i = 0; i < _parkAreaLines.Count; i++)
+      {
+        _parkAreaLines[i].material.color = color;
+      }
+    }
+  }
+}
